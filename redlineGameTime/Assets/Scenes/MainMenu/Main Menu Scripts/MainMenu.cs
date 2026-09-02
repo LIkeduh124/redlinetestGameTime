d4597124cd4a4d234c40190d255e2a1ddee95886
AHDood <noe.almanza@stu.daviess.kyschools.us>
2026-09-02 13:05:04 -0500

    Reorganized Button Code

--- redlineGameTime/Assets/Scenes/MainMenu/Main Menu Scripts/MainMenu.cs
+++ redlineGameTime/Assets/Scenes/MainMenu/Main Menu Scripts/MainMenu.cs
@@ -7,7 +7,9 @@ public class MainMenu : MonoBehaviour
     //A script made to open the Scene of the gameplay, once pressed.
     public void PlayGame()
     {
+        Debug.Log("PlayButton Works!");
         SceneManager.LoadScene("SampleScene");
+        
     }
     
 }
