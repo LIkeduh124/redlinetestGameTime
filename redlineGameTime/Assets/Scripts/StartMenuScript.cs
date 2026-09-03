88601b2ac1c31e129b87b9e18194089c3745b510
AHDood <noe.almanza@stu.daviess.kyschools.us>
2026-09-03 13:03:17 -0500

    added Quit Button

--- redlineGameTime/Assets/Scripts/StartMenuScript.cs
+++ redlineGameTime/Assets/Scripts/StartMenuScript.cs
@@ -4,9 +4,23 @@ using UnityEngine.SceneManagement;
 public class StartMenuScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   public void PlayGame()
+    public void PlayGame()
     {
         Debug.Log("Game Start button pressed");
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        // Logs a message in the console to prove it works while inside the Editor
+        Debug.Log("Player has quit the game!");
+
+#if UNITY_EDITOR
+        // This stops play mode if you are testing inside the Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        // This closes the actual built application (.exe, .app, etc.)
+        Application.Quit();
+
     }
 }
